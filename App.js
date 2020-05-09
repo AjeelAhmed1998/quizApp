4380c4bc0a18733c4b74272a4d576163c1a4e6dc
AjeelAhmed1998 <ajeelahmed1998@gmail.com>
2020-05-09 16:02:45 +0500

    mess

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Start from './src/Start';
-import { render } from 'react-dom';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
+import { render } from "react-dom";
 
-export default class App extends Component{
-  constructor(props){
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = {
+     
+    };
   }
 
-  render(){
-    return(
-      <Start/>
-    ); 
+  render() {
+    return (
+      <View style={style.container}>
+        <TouchableOpacity style={style.startContainer}>
+
+
+          <Text>
+            Start
+          </Text>
+
+        </TouchableOpacity>
+        
+        
+        
+      </View>
+    );
   }
-
-
-
-
 }
+
+
+const style = StyleSheet.create({
+  container: {
+
+    flex: 1, 
+    alignItems:"center",
+    backgroundColor: "red"
+
+  },
+  startContainer:{
+    flex: 1, 
+    alignItems: "center"
+  }, 
+  Header:{}
+});
